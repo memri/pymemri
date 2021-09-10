ace83847fec8fedf0e9108dfc7989c0cb15d54fc
Eelco van der Wel <eelcovdw@gmail.com>
2021-09-10 10:27:36 +0200

    qr flow send email

--- pymemri/client_simulator/qr_server.py
+++ pymemri/client_simulator/qr_server.py
@@ -1,10 +1,13 @@
+from pymemri.data.itembase import Edge
+from pymemri.plugin.schema import PluginRun
+from pymemri.pod.client import PodClient
 import flask
 import multiprocessing
 from flask import render_template
 from time import sleep
 import os
-from pymemri.plugin.states import RUN_USER_ACTION_NEEDED
-from pymemri.plugin.pluginbase import POD_PLUGIN_DNS_ENV
+from pymemri.plugin.states import RUN_USER_ACTION_NEEDED, RUN_USER_ACTION_COMPLETED
+from pymemri.plugin.pluginbase import POD_PLUGIN_DNS_ENV, PluginBase
 from pymemri.cvu.utils import get_default_cvu
 
 app = flask.Flask(__name__, template_folder='template')
@@ -24,7 +27,7 @@ def run_app(qr_dict, host="0.0.0.0", port=8080):
     qr_code_dict = qr_dict
     app.run(host=host, port=port)
 
-def run_qr_flow(_qr_code_data, client, plugin_run):
+def run_qr_flow(_qr_code_data, client: PodClient, plugin_run: PluginRun):
     manager = multiprocessing.Manager()
     process_dict = manager.dict()
     process_dict["qr_code"] = _qr_code_data
@@ -39,8 +42,18 @@ def run_qr_flow(_qr_code_data, client, plugin_run):
     print(f"GO TO {full_user_auth_url} and scan the code")
 
     # Gather email
-    # show cvu that gather email, and wait untill status = "ready"
-    # Cant do this because 1) authEmail is not defined in front-end 2) Don't have CVU's yet
+    plugin_run.status = RUN_USER_ACTION_NEEDED
+    pwd_cvu = get_default_cvu("request_password.cvu")
+    client.bulk_action(
+        create_items=[pwd_cvu], 
+        create_edges=[Edge(plugin_run, pwd_cvu, "view")]
+    )
+    client.update_item(plugin_run)
+
+    # Poll until status == "ready"
+    while plugin_run.status != RUN_USER_ACTION_COMPLETED:
+        sleep(1)
+        plugin_run = client.get(plugin_run.id)
 
     # send email
     try:
@@ -55,13 +68,14 @@ def run_qr_flow(_qr_code_data, client, plugin_run):
     
     # set 
     # depending on whether email was a success, either show qr link, or message that link was sent to email
-    cvu = get_default_cvu("qr_code_auth.cvu")
-    client.create(cvu)
-    plugin_run.add_edge("view", cvu)
-    client.create_edge(plugin_run.get_edges("view")[0])
-    plugin_run.status = RUN_USER_ACTION_NEEDED
-    plugin_run.authUrl = full_user_auth_url
-    client.update_item(plugin_run)
+    if not email_sent:
+        cvu = get_default_cvu("qr_code_auth.cvu")
+        client.create(cvu)
+        plugin_run.add_edge("view", cvu)
+        client.create_edge(plugin_run.get_edges("view")[0])
+        plugin_run.status = RUN_USER_ACTION_NEEDED
+        plugin_run.authUrl = full_user_auth_url
+        client.update_item(plugin_run)
     
     return process, process_dict
 
