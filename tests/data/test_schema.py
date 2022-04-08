613bf5bdc30ebbec9a2ab5dc82d16195b3b923ef
Eelco van der Wel <eelcovdw@gmail.com>
2022-04-08 15:47:13 +0200

    add central schema tests

---
+++ tests/data/test_schema.py
@@ -0,0 +1,18 @@
+import pytest
+import inspect
+from pymemri.data import _central_schema
+from pymemri.pod.client import PodClient
+
+@pytest.fixture
+def central_schema():
+    return {
+        k: v
+        for k, v in inspect.getmembers(_central_schema)
+        if inspect.isclass(v) and v.__module__ == "pymemri.data._central_schema"
+    }
+
+
+def test_add_to_schema(central_schema):
+    client = PodClient()
+    for k, v in central_schema.items():
+        assert client.add_to_schema(v), f"Could not add {k} to schema"
