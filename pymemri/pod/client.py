28cc787b83493fa36a160362efcdcfe27a43c3c0
Alp <alpdeniz@protonmail.com>
2022-06-26 20:39:22 +0300

    Conform to new edgelist (also enforced by pod) & update docs

--- pymemri/pod/client.py
+++ pymemri/pod/client.py
@@ -666,10 +666,12 @@ class PodClient:
 # Cell
 class Dog(Item):
     properties = Item.properties + ["name", "age", "bites", "weight"]
+    edges = Item.edges + ["friend"]
 
-    def __init__(self, name: str=None, age: int=None, bites: bool=False, weight: float=None, **kwargs):
+    def __init__(self, name: str=None, age: int=None, bites: bool=False, weight: float=None, friend: EdgeList["Person"]=None, **kwargs):
         super().__init__(**kwargs)
         self.name = name
         self.age = age
         self.bites = bites
         self.weight = weight
+        self.friend = EdgeList("friend", "Person", friend)
