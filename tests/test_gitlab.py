c76c0760f6294240af24fb83ef55531e5fe1d9a1
Aziz Berkay Yesilyurt <abyesilyurt@gmail.com>
2022-10-03 13:51:42 +0200

    parametrize tests

--- tests/test_gitlab.py
+++ tests/test_gitlab.py
@@ -5,29 +5,19 @@ from pymemri.pod.client import PodClient
 from pymemri.template.formatter import gitlab_slugify
 
 
-def test_gitlab_slugify():
-
-    # Lowercased
-    string = "SAMPLE Project"
-    assert gitlab_slugify(string) == "sample-project"
-
-    # First/Last Character is not a hyphen
-    string = "--sample space project--"
-    assert gitlab_slugify(string) == "sample-space-project"
-
-    # Maximum length is 63 bytes
-    string = "s" * 64
-    assert gitlab_slugify(string) == "s" * 63
-
-    # Anything not matching [a-z0-9-] is replaced with a -
-    string = "sample project"
-    assert gitlab_slugify(string) == "sample-project"
-
-    string = "sample_project"
-    assert gitlab_slugify(string) == "sample-project"
-
-    string = "01-09-test"
-    assert gitlab_slugify(string) == "01-09-test"
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        ("SAMPLE Project", "sample-project"),
+        ("--sample space project--", "sample-space-project"),
+        ("s" * 64, "s" * 63),
+        ("sample project", "sample-project"),
+        ("sample_project", "sample-project"),
+        ("01-09-test", "01-09-test"),
+    ],
+)
+def test_gitlab_slugify(input, expected):
+    assert gitlab_slugify(input) == expected
 
 
 def test_gitlab_missing_oauth():
